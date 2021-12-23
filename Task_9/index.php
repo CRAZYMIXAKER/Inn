9f9a5486b1d688bae1ee3bac9d1ab0e924342a28
CRAZYMIXAKER <47754210+CRAZYMIXAKER@users.noreply.github.com>
2021-12-23 17:35:42 +0300

    Update index.php

--- Task_9/index.php
+++ Task_9/index.php
@@ -2,11 +2,12 @@
 
 function sumThree(int $number, array $arr = [2, 7, 7, 1, 8, 2, 7, 8, 7]): array
 {
+    $output=[];
     for ($i = 0; $i < count($arr) - 2; $i++) {
         $firstNumber = $arr[$i];
         $secondNumber = $arr[$i + 1];
         $thirdNumber = $arr[$i + 2];
-        $number == ($firstNumber + $secondNumber + $thirdNumber) ? $output[] = [$firstNumber . ' + ' . $secondNumber . ' + ' . $thirdNumber . ' = ' . $number] : $output=[];
+        $number == ($firstNumber + $secondNumber + $thirdNumber) ? $output[] = [$firstNumber . ' + ' . $secondNumber . ' + ' . $thirdNumber . ' = ' . $number] : "";
     }
     return $output;
 }

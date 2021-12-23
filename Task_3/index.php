0f1924802d864b681941d555d345f796b5d43e63
CRAZYMIXAKER <zombisuki@gmail.com>
2021-12-23 13:53:56 +0300

    T_3

---
+++ Task_3/index.php
@@ -0,0 +1,19 @@
+<?php
+
+function digit(int $number): int
+{
+    do {
+        $numberToString = (string)$number;
+        $count = 0;
+
+        for ($i = 0; $i < strlen($numberToString); $i++) {
+            $count += (int)$numberToString[$i];
+        }
+
+        $number = (string)$count;
+    } while (strlen($number) > 1);
+
+    return $number;
+}
+
+digit(5689);

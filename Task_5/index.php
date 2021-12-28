4ef391ddb748d82c1b9b05f1fc97f9b90b02ce2c
CRAZYMIXAKER <47754210+CRAZYMIXAKER@users.noreply.github.com>
2021-12-28 04:57:44 +0300

    Update index.php

--- Task_5/index.php
+++ Task_5/index.php
@@ -4,8 +4,8 @@ function fib(int $n, array $arr = [1, 1]): float
 {
     do {
         $arr[] = $arr[count($arr) - 1] + $arr[count($arr) - 2];
-    } while (count($arr) <= $n);
-    return $arr[$n];
+    } while (floor(log10($arr[count($arr) - 1]) + 1) <= $n);
+    return $arr[count($arr) - 1];
 }
 
 echo fib(100);

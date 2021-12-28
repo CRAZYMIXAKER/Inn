a6a1ae01904597a5f3f65eaad9e62a656ffe28c8
CRAZYMIXAKER <47754210+CRAZYMIXAKER@users.noreply.github.com>
2021-12-28 06:29:06 +0300

    Update index.php

--- Task_2/index.php
+++ Task_2/index.php
@@ -2,7 +2,7 @@
 
 function countDaysToBirthday($dateBirthday)
 {
-    return date_diff((new DateTime())->setTime(00, 00), new DateTime($dateBirthday))->days;
+    return date_diff(new DateTime("00:00"), new DateTime($dateBirthday))->days;
 }
 
 echo countDaysToBirthday("2021-12-31") . ' day(-s) left until birthday';
